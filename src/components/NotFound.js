28bb168e246a0c753aee5ddc8fb1024226287fb1
Siddharthainturu1 <siddharthainturu@gmail.com>
2024-09-28 16:56:44 -0700

    commit

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,11 +1,30 @@
 // src/components/NotFound.js
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './NotFound.css'; // Optional for styling
 
 const NotFound = () => {
     return (
-        <div>
-            <h1>Welcome</h1>
-            <p>Click on the Header buttons</p>
+        <div className="not-found-container">
+            <h1>Welcome to the App!</h1>
+            <p>Explore the options in the header to navigate the site:</p>
+            <ul className="header-options">
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About the Project</Link>
+                </li>
+                <li>
+                    <Link to="/references">References</Link>
+                </li>
+                <li>
+                    <Link to="/conclusion">Conclusion</Link>
+                </li>
+                <li>
+                    <Link to="/quiz">Take a Quiz</Link>
+                </li>
+            </ul>
         </div>
     );
 };
